0116ce1b19d8b61063916eb98b4032a9ca89d8f6
Baji Pathan <bajibpathan@gmail.com>
2025-05-25 00:17:20 +0000

    Updated Prime number script

--- 04.prime-number.sh
+++ 04.prime-number.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 source ./colors.sh
 
+sudo dnf module list cowsay &> /dev/null
+if [ $? -ne 0 ]; then
+    sudo dnf install cowsay -y &> /dev/null
+fi
+
 # Banner 
-echo "+-------------------------------+"
-echo -e " $MAGENTA IST IT A PRIME ??? $DEFAULT "     
-echo "+-------------------------------+"
+echo "+------------------------------------+"
+echo -e " $MAGENTA IS IT A PRIME ??? LET'S FIND OUT $DEFAULT "     
+echo "+------------------------------------+"
 
 # Input from user
 read -p "Enter a number to find out: " NUMBER
@@ -12,21 +17,18 @@ read -p "Enter a number to find out: " NUMBER
 # If the given number is 0 or 1,then it is not a prime number
 if [ $NUMBER -le 1 ]
 then
-    echo -e "$YELLOW $1 is not a Prime Number"
-    exit 1
+   cowsay "$NUMBER is not a Prime Number :("
 fi
 
 LOOP_VALUE=`expr $NUMBER / 2`
 echo "                     "
-
 # Looping the given number to find out if it is Prime or not
 for counter in $(seq 2 $LOOP_VALUE)
 do
     if [ `expr $NUMBER % $counter` -eq 0 ]
     then
-        echo -e "$YELLOW $NUMBER $DEFAULT is not a Prime Number"
-        exit
+        cowsay "$NUMBER is not a Prime Number :("
+        exit 1
     fi
 done
-
-echo -e "$GREEN $NUMBER $DEFAULT is a Prime Number"
+cowsay "$NUMBER is a Prime NUmber :)"
